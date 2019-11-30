edcfad125ff7a97d5706bbc4de1bee4e9681339d
ryandaryl <ryandarylmills@gmail.com>
2019-11-30 18:26:13 +1100

    Save a global variable on initialisation.

--- celery_worker.py
+++ celery_worker.py
@@ -1,5 +1,7 @@
 import os
 from celery import Celery
+from celery.signals import worker_process_init
+from billiard import current_process
 broker = os.environ['REDIS_URL']
 backend = os.environ['REDIS_URL']
 name = os.environ.get('CELERY_NAME', 'default_name')
@@ -7,7 +9,16 @@ name = os.environ.get('CELERY_NAME', 'default_name')
 celery = Celery(name, broker=broker,
                 backend=backend)
 
+my_global_var = None
+
+@worker_process_init.connect()
+def on_worker_init(**_):
+    import datetime
+    global my_global_var
+    my_global_var = datetime.datetime.now()
+
 @celery.task(name='celery_worker.test', bind=True)
 def test(self, data):
+    current_process_index = current_process().index
     task_id = self.request.id
-    return '{} test OK.'.format(task_id)
+    return '{}: {} test OK.'.format(current_process_index, task_id)
